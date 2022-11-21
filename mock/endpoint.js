64d52f37b63b8743ebb29b151ee4f95e32d837ce
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2022-11-21 23:08:01 +0100

    styling

--- mock/endpoint.js
+++ mock/endpoint.js
@@ -5,13 +5,13 @@ export default [
     response: () => {
       return [
         {
-          tittel: "Taddddddda!!",
+          tittel: "Søknad om dagpenger, permittert",
           link: "https://www.dev.nav.no/minside/",
           opprettet: "2020-03-13T08:53:24.636Z",
           sistEndret: null,
         },
         {
-          tittel: "Taddddddda!!",
+          tittel: "Søknad om foreldre- og svangerskapspenger",
           link: "https://www.dev.nav.no/minside/",
           opprettet: "2020-03-13T08:53:24.636Z",
           sistEndret: "2020-03-14T08:53:24.636Z",
